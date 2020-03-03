d56559f17713731262fe5e6195e837695fe19202
Danila Papko <36470704+dapapko@users.noreply.github.com>
2020-03-03 16:21:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <bitset>
 
 using namespace std;
 
 void dumper(vector<char>* dump, unsigned short int value, int base) {
-    short int mask = 0b1111;
-    int base_shift = log2(base);
+    short int mask = 0xf;
+    if(base == 2) mask >>=3;
+    int base_shift = 4;
+    if(base == 2) base_shift = 1;
     int correction_shift = 0;
-    int upperbound = 16 / base_shift;
-    if(base == 3) mask <<=1;
-    else if (base == 1) mask <<=3;
+    int upperbound = 4;
+    if(base == 2) upperbound = 16;
     for(int i =0; i < upperbound; i++) {
         int digit = (value & mask) >> correction_shift;
         mask <<= base_shift;
@@ -38,7 +38,7 @@ vector<char> fdumper(T value, int base) {
 
 template <typename T>
 void get_inner_representation(T value) {
-    int firstbound, secondbound, int sign_index;
+    int firstbound, secondbound, sign_index;
     T* pValue = &value;
     auto* p = reinterpret_cast<long long int*>(pValue);
     bitset<64> dump = bitset<64>(*p);
@@ -51,17 +51,34 @@ void get_inner_representation(T value) {
         secondbound = 30;
         sign_index = 31;
     }
-    cout << "Sign: " << dump[sign_index] << endl;
-    cout  << "Exponent: ";
+    cout << dump[sign_index] << ' ';
     for(int i = secondbound; i >firstbound ; i--)
         cout << dump[i];
-    cout << endl << "Mantis: ";
+    cout << ' ';
     for(int i = firstbound; i > 0; i--)
         cout << dump[i];
 }
 
 
+template<typename T>
+T bin_to_number(vector<char> dump) {
+    unsigned long long int digit, val;
+    val = 0;
+    int correction_shift = dump.size() - 1;
+    for(char c: dump) {
+        c == '0' ? digit = 0 : digit = 1;
+        val |= (digit << correction_shift);
+        correction_shift--;
+    }
+    T rvalue = *reinterpret_cast<T*>(&val);
+    return rvalue;
+}
+
+
 int main() {
-    float a = 1.5;
-    get_inner_representation(a);
+    float f = 12.34;
+    std::vector<char> dump = fdumper(f, 2);
+    get_inner_representation(f);
+    float a = bin_to_number<float>(dump);
+    cout << endl << a;
 }
