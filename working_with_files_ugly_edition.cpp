c883e418bd2d8fbf8292197735fe3b03e0a10f43
Danila Papko <36470704+dapapko@users.noreply.github.com>
2020-03-15 18:37:07 +0300

    Update working_with_files_ugly_edition.cpp

--- working_with_files_ugly_edition.cpp
+++ working_with_files_ugly_edition.cpp
@@ -3,58 +3,57 @@
 using namespace std;
 
 
-template<typename T>
-int* dumper(T number) {
-    int* dump = new int[sizeof(T)*8];
-    unsigned long long int value = *reinterpret_cast<unsigned long long int *>(&number);
+template <typename T>
+int* dumper(T a) {
+    int size = sizeof(T)*8;
+    int* dump = new int[size];
+    auto val = *reinterpret_cast<unsigned long long int*>(&a);
     unsigned long long int mask = 1;
     int digit;
-    int correction_shift = 0;
-    for (int i = sizeof(T) * 8 - 1; i >= 0; i--) {
-        digit = (value & mask) >> correction_shift;
-        dump[i] = digit;
-        correction_shift++;
+    int shift = 0;
+    for (int i = size - 1; i >=0; i--) {
+        digit = (val & mask) >> shift;
+        shift++;
         mask <<= 1;
+        dump[i] = digit;
     }
     return dump;
 }
 
 
 template <typename T>
-void write_array_to_file(int array[]) {
-    fstream out("dump.txt");
-    if (!out.is_open())
-    {
-        cout << "Writing error";
+void write(int* arr) {
+    fstream out("file.txt", ios::out);
+    if (!out.is_open()) {
+        cout << "File can't be opened." << endl;
         exit(1);
     }
-    for(int i=0; i < sizeof(T)*8;i++)
-        out << array[i];
+    for (int i = 0; i < sizeof(T) * 8; i++) {
+        out << arr[i];
+    }
     out.close();
 }
 
-
 template <typename T>
-void read_from_file(){
-    char* content = new char[sizeof(T)*8];
-    fstream dump_file("dump.txt");
-    if (!dump_file.is_open())
-    {
-        cout << "Reading error";
+void read_from_file() {
+    int size = sizeof(T)*8;
+    fstream file("dump.txt");
+    if (!file.is_open()) {
+        cout << "File can't be opened." << endl;
         exit(1);
     }
-    dump_file.getline(content, sizeof(T)*8+1), '/0';
-    dump_file.close();
-    for(int i=0; i < sizeof(T)*8;i++)
-        cout << content[i];
-    delete[] content;
+    char* arr = new char[size];
+    for (int i = 0; i < size; i++)
+        cout << arr[i];
+    dump.close();
 }
 
-
 int main() {
-    double n = -23.65;
-    int* dump = dumper(n);
-    write_array_to_file<double>(dump);
-    read_from_file<double>();
-    delete[] dump;
+    float d = -23.65;
+    int* dump = toDump(d);
+    for(int i=0; i < sizeof(d)*8; i ++)
+        cout << dump[i];
+    cout << endl;
+    write_to_file<float>(dump);
+    read_from_file<float>();
 }
